b67d76206016e6528e134e79015906ec2155103e
Ian Lucca Soares Mesquita <89036876+IanLucca12@users.noreply.github.com>
2023-01-16 20:28:23 -0300

    Add files via upload

---
+++ parte 2.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+struct dados{
+    char category[40];
+    float amount;
+};
+
+void insertionSort(float list[], int n,int o[]){
+    int i, j;
+    float next;
+    for (i=1; i<n; i++) {
+        next = list[i];
+        for (j=i-1; j>=0&&next>list[j];j--){
+            list[j+1] = list[j];
+            o[i]=j;
+        }
+        list[j+1] = next;
+    }
+
+ }
+
+ FILE acessLine(FILE *fp, int line){
+    char ch;
+    int count = 0;
+    while(count<line){
+        ch = fgetc(fp);
+        if(ch == '\n'){
+            count = count+1;
+        }
+    }
+    return *fp;
+}
+
+ int main(){
+    struct dados D;
+    float lis[129000];
+    int line,j,o[129000], num=0;
+    FILE *fp, *fp2;
+    FILE *copy;
+    fp = fopen ( "Amazonsale.csv", "r" ) ;
+    if ( fp == NULL ){
+        puts ( "Cannot open file" ) ;
+        system("pause");
+        exit(0) ;
+    }
+    while(1){
+        char result;
+        result = fscanf(fp, "%s %f", D.category, &D.amount);
+        lis[num]=D.amount;
+        num=num+1;
+        if ( result == EOF)
+            break;
+            //printf("%s - %.2f \n", D.category,D.amount);
+    }
+    fclose(fp);
+    copy = fopen("Amazonsalecopy.csv","w");
+    insertionSort(lis,num,o);
+    printf("\n ORDENADO: \n");
+    for(j=0;j<=num;j++){
+            fp2 = fopen("Acess_Line.csv", "w");
+            if ( fp2 == NULL ){
+                puts ( "Cannot open file" ) ;
+                system("pause");
+                exit(0) ;
+            }
+            line=o[j];
+            *fp = acessLine(fp, line);
+            char ch = fgetc(fp);
+
+            while(ch !=' '){
+                fputc(ch,fp2);
+                ch = fgetc(fp);
+            }
+
+        //printf("%d\n",o[j]);
+        printf("%.2f\n",lis[j]);
+        fprintf(copy,"%.2f\n",lis[j]);
+    fclose(copy);
+    fclose(fp);
+    }
+}
